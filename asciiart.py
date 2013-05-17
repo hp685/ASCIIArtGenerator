def45648b64764f2ab7452f1917bef564683ed4b
Harsh <harsh@nyu.edu>
2013-05-17 02:18:19 -0400

    V1

---
+++ asciiart.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import Image
+import ImageOps
+
+#Parse
+op = OptionParser()
+
+op.add_option("-I", "--image", dest="_image",
+                  help="Read an image")
+
+(options, args) = op.parse_args()
+
+#Thumbnail
+size =120,120
+#Read image
+im   = Image.open(options._image)
+imgr = ImageOps.grayscale(im)
+imgr.thumbnail(size, Image.ANTIALIAS)
+imgr.save("temp","JPEG")
+px = imgr.load()
+f = open('out','w')
+
+xsize, ysize = imgr.size
+
+il = []
+
+#hash
+for i in range(1,ysize):
+    l = []
+    for j in range(1,xsize):
+        l.append(chr(64 + (px[j,i] + (20 - px[j,i] % 20)) % 34))
+    il.append(l)
+for item in il:
+    f.write("%s\n" % "".join(item))
+
+for item in il:
+    print "".join(item)
+
+print xsize, ysize
