97fbe80fd31fddf2eeff7968ce12057ea092542c
hp685 <harsh@nyu.edu>
2019-02-07 23:18:27 -0800

    Update requirements

--- asciiart.py
+++ asciiart.py
@@ -2,14 +2,14 @@
 
 import sys
 from optparse import OptionParser
-import Image
-import ImageOps
+from PIL import Image
+from PIL import ImageOps
 
-#Parse
+# Parse
 op = OptionParser()
 
 op.add_option("-I", "--image", dest="_image",
-                  help="Read an image")
+              help="Read an image")
 
 (options, args) = op.parse_args()
 
@@ -17,9 +17,9 @@ if len(sys.argv) != 3:
     print op.print_help()
     sys.exit()
 
-#Thumbnail
-size =  128, 128
-#Read image
+# Thumbnail
+size = 128, 128
+# Read image
 try:
     im = Image.open(options._image)
 except:
@@ -28,19 +28,19 @@ except:
 
 imgr = ImageOps.grayscale(im)
 imgr.thumbnail(size, Image.ANTIALIAS)
-imgr.save("temp","JPEG")
+imgr.save("temp", "JPEG")
 px = imgr.load()
-f = open('out','w')
+f = open('out', 'w')
 
 xsize, ysize = imgr.size
 
 il = []
 
-#hash
-for i in range(1,ysize):
+# hash
+for i in range(1, ysize):
     l = []
-    for j in range(1,xsize):
-        l.append(chr(64 + (px[j,i] + (30 - px[j,i] % 30)) % 34))
+    for j in range(1, xsize):
+        l.append(chr(64 + (px[j, i] + (30 - px[j, i] % 30)) % 34))
     il.append(l)
 
 for item in il:
